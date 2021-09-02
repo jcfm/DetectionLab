8c6a3968ac8e7b37cbecec34495e982af2c16f5b
Jake <magee.jake@gmail.com>
2021-09-02 23:52:29 +0100

    Update siembol_bootstrap.sh

--- Vagrant/siembol_bootstrap.sh
+++ Vagrant/siembol_bootstrap.sh
@@ -47,4 +47,24 @@ metadata:
 spec:
   ca:
     secretName: cacerts
+    
+
+echo "************************************************************"
+echo "****************** Installing dependencies *****************"
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add gresearch https://g-research.github.io/charts
+helm repo update
+
+helm install siembol-zookeeper bitnami/zookeeper -n=siembol
+
+helm install kafka bitnami/kafka -n=siembol
+
+helm install storm gresearch/storm -n=siembol --set supervisor.replicaCount=1
+
+
+echo "************************************************************"
+echo "Checking status by running: 'kubectl get pods -n siembol'"
+kubectl get pods -n siembol
+echo "************************************************************"
+
 EOF
